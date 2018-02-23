77136761ae2f20527c2b1d1c0484835e7cd073c7
Ge Wang <wg.wangge1984@gmail.com>
2018-02-23 00:03:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,13 +5,10 @@ var recipes = {
 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  
+
 }
-
-
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
 }
- 
